4c75606ac1fee80aa9b3709daca799713e901520
Mathon <cnmathon@gmail.com>
2022-12-30 14:54:07 +0800

    perf(home-view): clean up unnecessary logic of HomeView

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,65 +2,8 @@
 name: Home
 </route>
 
-<script setup lang="ts">
-import { useRouter } from 'vue-router'
-import { useAppStore } from '@/stores'
-
-const router = useRouter()
-const appStore = useAppStore()
-
-const changeLocale = () => {
-  const NEW_LOCALE = appStore.locale === 'zhCN' ? 'enUS' : 'zhCN'
-  appStore.changeLocale(NEW_LOCALE)
-}
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div class="home-view">
-    <a-button
-      class="home-view__button"
-      type="primary"
-      size="large"
-      block
-      @click="() => router.push('/counter')"
-    >
-      {{ $t('Navigate To New Page') }}
-    </a-button>
-    <a-button
-      class="home-view__button"
-      type="primary"
-      size="large"
-      block
-      @click="changeLocale"
-    >
-      {{ $t('Change Language') }}
-    </a-button>
-    <a-button
-      class="home-view__button"
-      type="primary"
-      size="large"
-      block
-      @click="() => router.push('/sfcI18N')"
-    >
-      SFC I18N {{ $t('Test') }}
-    </a-button>
-    <a-button
-      class="home-view__button"
-      type="primary"
-      size="large"
-      block
-      @click="() => router.push('/mock')"
-    >
-      {{ $t('Navigate To New Mock') }}
-    </a-button>
-  </div>
+  <div class="home-view"></div>
 </template>
-
-<style lang="less">
-.home-view {
-  padding: 10px;
-  &__button {
-    margin-bottom: 10px;
-  }
-}
-</style>
